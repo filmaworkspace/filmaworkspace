1a3e41c71224991657a81e679f2f50e681e28d6e
filmaworkspace <filmaworkspace@proton.me>
2026-06-03 23:35:02 +0200

    Add files via upload

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Inter } from "next/font/google";
 import { Eye, EyeOff, AlertCircle, ArrowRight } from "lucide-react";
-import Image from "next/image";
 import { auth } from "@/lib/firebase";
 import { signInWithEmailAndPassword, setPersistence, browserLocalPersistence, browserSessionPersistence } from "firebase/auth";
 
