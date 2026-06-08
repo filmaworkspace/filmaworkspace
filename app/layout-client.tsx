e8518b38f5da555f3e2aa73f5a8c8728761d3d6d
Filma Workspace <filmaworkspace@proton.me>
2026-06-08 13:35:32 +0200

    Update layout-client.tsx

--- app/layout-client.tsx
+++ app/layout-client.tsx
@@ -1,33 +1,26 @@
 "use client";
-
 // ─── Framework ────────────────────────────────────────────────────────────────
 import { usePathname } from "next/navigation";
-
 // ─── Internal ────────────────────────────────────────────────────────────────
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { UserProvider } from "@/contexts/UserContext";
-
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-
   const isAuthPage =
     pathname === "/" ||
     pathname === "/login" ||
     pathname === "/register" ||
     pathname === "/forgot-password";
-
   return (
     <UserProvider>
       <div className="flex flex-col min-h-screen">
         {!isAuthPage && <Header />}
-        <main className={`flex flex-col flex-grow ${!isAuthPage ? "pt-[3.5rem]" : ""}`}>
-          {children}
-        </main>
+        <main className="flex flex-col flex-grow">{children}</main>
         {!isAuthPage && <Footer />}
       </div>
     </UserProvider>
